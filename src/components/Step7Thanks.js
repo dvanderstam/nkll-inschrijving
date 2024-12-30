1fab53e92b384c075c107d4e5e18d791ae9a9c8b
d.vanderstam@radarvertige.nl <d.vanderstam@radarvertige.nl>
2024-12-30 13:29:37 +0100

    title bedankt

--- src/components/Step7Thanks.js
+++ src/components/Step7Thanks.js
@@ -10,11 +10,11 @@ const Step7Thanks = ({ title }) => {
   const { resetRegistrationData } = useRegistration();
   const navigate = useNavigate();
   const CloseRegbutton = () => {
-    window.location.href = 'https://www.nklittleleague.nl'; // Navig ate to the new URL
+    window.location.href = 'https://www.nklittleleague.nl'; // Navigate to the new URL
   };
 
   useEffect(() => {
-    document.title = title;
+    document.title = 'Bedankt voor je inschrijving!';
   }, [title]);
 
   const handleNewRegistration = () => {
